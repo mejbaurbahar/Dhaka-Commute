f984f2b744d1d17202c0a1ab981e531fec5ccdd8
fagun <fagun@localhost>
2025-12-22 21:58:20 +0600

    Fix white space at bottom of bus details page

--- App.tsx
+++ App.tsx
@@ -2397,7 +2397,7 @@ const App: React.FC = () => {
         )}
 
         {/* Scrollable Content */}
-        <div className={`flex-1 p-4 space-y-4 ${selectedTrip ? 'pt-4 md:pt-20' : 'pt-[90px] md:pt-20'} pb-4`}>
+        <div className={`flex-1 p-4 space-y-4 bg-slate-50 dark:bg-slate-900 ${selectedTrip ? 'pt-4 md:pt-20' : 'pt-[90px] md:pt-20'} pb-4`}>
 
           {/* Stats Grid */}
           <div className="grid grid-cols-3 gap-3">
